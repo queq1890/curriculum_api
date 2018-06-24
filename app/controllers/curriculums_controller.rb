931745a65a85020938474ec57f880ce3bc041a85
queq1890 <32263803+queq1890@users.noreply.github.com>
2018-06-24 16:14:10 +0900

    set up rubocop and basic apis

--- app/controllers/curriculums_controller.rb
+++ app/controllers/curriculums_controller.rb
@@ -1,11 +1,43 @@
 class CurriculumsController < ApplicationController
+  before_action :set_curriculum, only: %i(show update destroy)
+
+  def index
+    @curriculums = Curriculum.all
+    render json: @curriculums
+  end
+
+  def show
+    render json: @curriculum
+  end
 
   def create
+    @curriculum = Curriculum.new(curriculum_params)
+    if @curriculum.save
+      render json: @curriculum, status: :created, location: @curriculum
+    else
+      render json:  @curriculum.errors, status: :unprocessable_entity
+    end
   end
 
   def update
+    if @curriculum.update(user_params)
+      render json: @curriculum
+    else
+      render json:  @curriculum.errors, status: :unprocessable_entity
+    end
   end
 
   def destroy
+    @curriculum.destroy
+  end
+
+  private
+
+  def set_curriculum
+    @curriculum = Curriculum.find(params[:id])
+  end
+
+  def curriculum_params
+    params.require(:curriculum).permit(:body)
   end
 end
